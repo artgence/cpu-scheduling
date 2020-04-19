27f580c815d2da4e6d7291d44699235faea3ec9d
artgence <461369561@qq.com>
2020-04-19 16:53:09 -0700

    test absolute path

--- src/main/TestPath.java
+++ src/main/TestPath.java
@@ -1,7 +1,21 @@
-//package main;
-//
-//public class TestPath {
-//    public static void main(String[] args) {
-//        System.out.println(System.getProperty("user.dir"));
-//    }
-//}
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TestPath {
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.getProperty("user.dir"));
+        File file = new File("/home/circleci/project/src/main/resources/CPU Scheduling.fxml");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = "";
+        String allcase = "";
+        while ((line = br.readLine())!= null){
+            allcase = allcase + line;
+        };
+        br.close();
+        System.out.println(allcase);
+    }
+}
