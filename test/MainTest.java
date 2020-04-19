e044c508bf7ca550edbd6ea2edbfab3acbb59c84
artgence <461369561@qq.com>
2020-04-19 16:23:55 -0700

    add TestPath.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.loadui.testfx.GuiTest.find;
 
+
 public class MainTest extends ApplicationTest {
 
     @Override
