ca9145aaa45427effe0bc2c39ed8badc1514e579
artgence <461369561@qq.com>
2020-04-19 16:27:51 -0700

    use absolute path

--- src/main/TestPath.java
+++ src/main/TestPath.java
@@ -1,7 +1,7 @@
-package main;
-
-public class TestPath {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-    }
-}
+//package main;
+//
+//public class TestPath {
+//    public static void main(String[] args) {
+//        System.out.println(System.getProperty("user.dir"));
+//    }
+//}
